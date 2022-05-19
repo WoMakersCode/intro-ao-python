88231315406decfad6a5618b8c8bb70af243e775
clasimoes <clasimoesr@gmail.com>
2022-05-19 10:18:16 -0300

    Melhorando o enunciado do desafio 14

--- aula07/07_desafio_14.py
+++ aula07/07_desafio_14.py
@@ -2,15 +2,12 @@ import sys
 import requests
 import json
 
-# Crie uma aplicação que lê na linha de comando o nome de um feitiço
-
-
-# Utilize a biblioteca requests para ler o JSON disponível em
+# Crie uma aplicação que permita ao usuário obter informações sobre um determinado feitiço.
+# O usuário deve digitar o nome de um feitiço como entrada para a nossa aplicação através da
+# linha de comando. Por exemplo:
+# python 07_desafio_14.py Vanishing Spell
+# Você deverá obeter os dados sobre o feitiço diretamente do JSON disponível em
 # https://construdelas.blob.core.windows.net/intro-ao-python/feiticos.json
-
-
-# Converta o CSV para um dicionário cujas chaves são as palavras do cabeçalho do CSV.
-
-
-# Imprima para o usuário os dados sobre o feitiço que ele solicitou.
+# Utilize a biblioteca requests para ler o JSON.
+# Ao final do programa, imprima os dados sobre o feitiço que ele solicitou.
 # Se o feitiço não foi encontrado, lance uma exceção.
